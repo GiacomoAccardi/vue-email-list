3bc610fa2a12a09cb0db8a0c30bbfa753ebe0f53
giacomohrd <giacomo.accardi.lavoro@gmail.com>
2024-07-25 15:47:52 +0200

    [FIX] script now works properly

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ createApp({
     data() {
         return { 
             random_mails: [],
-            email: '',
         }
     },
     methods: {
         generateRandomMail(){
+            this.random_mails = [];
             for (let i = 0; i < 10; i++) {
                 axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((response) => {
                     this.random_mails.push(response.data.response)
